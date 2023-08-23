9262261438af59e24dc3e12759b71abf58e074f9
Gilson Bazileu <gilsonbazileu@gmail.com>
2023-08-23 20:10:30 -0300

    problema 1036 resolvido

--- 1036 Bhaskara_s Formula.py
+++ 1036 Bhaskara_s Formula.py
@@ -7,22 +7,12 @@ c = float(c)
 
 delta = (b**2)-(4*a*c)
 
+
 if delta <0 or a ==0:
-    print("Impossível calcular")
-    
-elif delta >0:
+    print("Impossivel calcular")
+
+else :
     x1 = (-b + delta**0.5) / (2*a)
     x2 = (-b - delta**0.5) / (2*a)
-    print(f'R1 = {x1:.5f} \n R2 = {x2:.5f}')
-
-
-
-
-
-
-
-
-
-
-
-
+    print(f'R1 = {x1:.5f}')
+    print(f'R2 = {x2:.5f}')
