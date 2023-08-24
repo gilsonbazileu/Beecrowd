195e96ee457e465c957b07a6c413a9c32a4afad4
Gilson Bazileu <gilsonbazileu@gmail.com>
2023-08-23 22:25:10 -0300

    problema 1038 Snack OK

--- 1038 Snack.py
+++ 1038 Snack.py
@@ -1,15 +1,11 @@
-code,amount = input().split()
-
-
+code,amount = input().split() # se code 5
 
 pricelist = [4.00,4.50,5.0,2.0,1.50]
 
-index = int(code) -1  #0
+index = int(code) -1  #4
 
-price = pricelist[index]
+price = pricelist[index] #  price vai ser 1.50 que está na posiçao 4
 
-final_price = price * int(amount)
+final_price = price * int(amount) #
 
-print(f'Valor a pagar {final_price}' )
-
-
+print(f'Total: R$ {final_price:.2f}')
